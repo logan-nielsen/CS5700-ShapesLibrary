fd0629e27e2b563516fad387e63e075d3e4ea6cf
Logan Nielsen <loganskier@gmail.com>
2025-06-25 19:10:31 -0600

    Implement Line

---
+++ src/main/kotlin/Line.kt
@@ -0,0 +1,19 @@
+package org.example
+
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class Line(val point1: Point, val point2: Point): Shape() {
+    fun getSlope(): Double {
+        return (point2.y - point1.y) / (point2.x - point1.x)
+    }
+
+    fun getLength(): Double {
+        return sqrt((point2.x - point1.x).pow(2.0) + (point2.y - point1.y).pow(2.0))
+    }
+
+    override fun move(xDelta: Double, yDelta: Double) {
+        point1.move(xDelta, yDelta)
+        point2.move(xDelta, yDelta)
+    }
+}
